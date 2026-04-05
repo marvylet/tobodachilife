bc9d346e75cfae797bce8ba80ace5e02b05e7299
damia <dambibimbus@gmail.com>
2026-04-05 15:24:54 +0200

    feat: přidány wireframy + color scheme

---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+export interface Colors {
+    mainCol: "#FFFBF1",
+    secondCol: "#FFF2D0",
+    borderCol: "#3a362f",
+    buttonCol: "#FFB2B2",
+    bigTextCol: "#E36A6A",
+    textCol: "#250e0e"
+}
+
