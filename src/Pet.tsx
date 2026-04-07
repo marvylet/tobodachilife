008ab3213b536452a5a99f24bc9dec591602448f
Marv <marvylet6@volny.cz>
2026-04-07 08:51:50 +0200

    feat: useEffect v Pet

--- src/Pet.tsx
+++ src/Pet.tsx
@@ -1,4 +1,10 @@
+import {useState} from "react";
+
 function Pet () {
+    const [hlad, setHlad] = useState();
+    const [energie, setEnergie] = useState();
+    const [stesti, setStesti] = useState();
+
     return (
         <>
         </>
