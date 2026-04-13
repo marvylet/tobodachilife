a879eb50cd65ecb1f4d822ce2c685a90cad6880f
Marv <marvylet6@volny.cz>
2026-04-14 00:46:48 +0200

    feat: pridana logika hladu mazlicka

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css'
+import Pet from './Pet.tsx'
+import {useState} from "react";
 
 function App() {
+  const maxHlad = 50;
+  const [hlad, setHlad] = useState(maxHlad);
 
   return (
     <>
+      <Pet maxHlad={maxHlad} setHlad={setHlad} hlad={hlad} />
     </>
   )
 }
--- src/Pet.tsx
+++ src/Pet.tsx
@@ -1,12 +1,42 @@
-import {useState} from "react";
 
-function Pet () {
-    const [hlad, setHlad] = useState();
-    const [energie, setEnergie] = useState();
-    const [stesti, setStesti] = useState();
+interface PetProps {
+    maxHlad: number;
+    hlad: number;
+    setHlad: (hlad: number) => void;
+
+    /*
+    maxEnergie: number;
+    energie: number;
+    setEnergie: (energie: number) => void;
+
+    maxStesti: number;
+    stesti: number;
+    setStesti: (stesti: number) => void;
+     */
+}
+
+function Pet (props: PetProps) {
+    let text = "";
+
+    function UpdateHlad(){
+        if(props.hlad > props.maxHlad){
+            props.setHlad(props.maxHlad);
+        }
+
+        if((props.hlad - 2) <= 0){
+            props.setHlad(0);
+            text = "Game Over!"
+        }else{
+            props.setHlad(props.hlad - 2);
+            text = String(props.hlad);
+        }
+    }
+
+    setInterval(UpdateHlad, 15000);
 
     return (
         <>
+            <p>{props.maxHlad}/{text}</p>
         </>
     )
 }
