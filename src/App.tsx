e6d5637649e7cd31238fbf409f525de6aeb32a19
Marv <marvylet6@volny.cz>
2026-04-14 08:43:49 +0200

    feat: oprava logiky hladu, stesti a energie mazlicka

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Pet from './Pet.tsx'
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
   const maxHlad = 50;
@@ -11,6 +11,63 @@ function App() {
 
   const maxStesti = 50;
   const [stesti, setStesti] = useState(maxStesti);
+
+  //let ending = false;
+
+  function UpdateHlad(){
+    if(hlad > maxHlad){
+      setHlad(maxHlad);
+    }
+      if ((hlad - 2) <= 0) {
+        setHlad(0);
+        //ending = true;
+      } else {
+        setHlad(hlad - 2);
+      }
+  }
+  useEffect(() => {
+    const tick = setTimeout(() => {
+      UpdateHlad();
+    }, 5000);
+    return () => clearTimeout(tick);
+
+  }, [hlad]);
+
+  function UpdateEnergie(){
+    if(energie > maxEnergie){
+      setEnergie(maxEnergie);
+    }
+      if ((energie - 2) <= 0) {
+        setEnergie(0);
+      } else {
+        setEnergie(energie - 2);
+    }
+  }
+  useEffect(() => {
+    const tick = setTimeout(() => {
+      UpdateEnergie();
+    }, 2500);
+    return () => clearTimeout(tick);
+
+  }, [energie]);
+
+  function UpdateStesti(){
+    if(stesti > maxStesti){
+      setStesti(maxStesti);
+    }
+      if ((stesti - 2) <= 0) {
+        setStesti(0);
+      } else {
+        setStesti(stesti - 2);
+      }
+  }
+  useEffect(() => {
+    const tick = setTimeout(() => {
+      UpdateStesti();
+    }, 3000);
+    return () => clearTimeout(tick);
+
+  }, [stesti]);
 
   return (
     <>
--- src/Pet.tsx
+++ src/Pet.tsx
@@ -1,5 +1,3 @@
-import {useState} from "react";
-
 interface PetProps {
     maxHlad: number;
     hlad: number;
@@ -15,70 +13,9 @@ interface PetProps {
 }
 
 function Pet (props: PetProps) {
-    let ending = false;
-    const [endText, setEndText] = useState("Playing...");
-
-    function UpdateHlad(){
-        if(props.hlad > props.maxHlad){
-            props.setHlad(props.maxHlad);
-        }
-
-        if(ending) {
-            props.setHlad(0);
-        }else {
-            if ((props.hlad - 2) <= 0) {
-                props.setHlad(0);
-                ending = true;
-            } else {
-                props.setHlad(props.hlad - 2);
-            }
-        }
-    }
-    setInterval(UpdateHlad, 15000);
-
-    function UpdateEnergie(){
-        if(props.energie > props.maxEnergie){
-            props.setEnergie(props.maxEnergie);
-        }
-
-        if(ending) {
-            props.setEnergie(0);
-        }else {
-            if ((props.energie - 2) <= 0) {
-                props.setEnergie(0);
-                ending = true;
-            } else {
-                props.setEnergie(props.energie - 2);
-            }
-        }
-    }
-    setInterval(UpdateEnergie, 15000);
-
-    function UpdateStesti(){
-        if(props.stesti > props.maxStesti){
-            props.setStesti(props.maxStesti);
-        }
-
-        if(ending) {
-            props.setStesti(0);
-        }else {
-            if ((props.stesti - 2) <= 0) {
-                props.setStesti(0);
-                ending = true;
-            } else {
-                props.setStesti(props.stesti - 2);
-            }
-        }
-    }
-    setInterval(UpdateStesti, 15000);
-
-    if(ending){
-        setEndText("Game Over!");
-    }
 
     return (
         <>
-            <p>{endText}</p>
             <p>Hlad: {props.maxHlad}/{props.hlad}</p>
             <p>Energie: {props.maxEnergie}/{props.energie}</p>
             <p>Stesti: {props.maxStesti}/{props.stesti}</p>
